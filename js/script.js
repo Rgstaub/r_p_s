b9d355a379dc5eaa03752f6e58d2abd28eb2317f
Rgstaub <rgstaub@gmail.com>
2017-07-10 23:47:29 -0400

    frameworks for local and server storage are working.

--- js/script.js
+++ js/script.js
@@ -8,9 +8,10 @@ Connect the two players to the app
 
 Get the player input and hold it until both players have submitted
 
-
+1
 
 */
+//=============================================================================
 
 // Initialize Firebase
 var config = {
@@ -24,17 +25,87 @@ var config = {
 
 firebase.initializeApp(config);
 
-
+// Set global variables
 var database = firebase.database();
 var clickCounter = 0;
-var scoreboard = $('#testTitle')
+var scoreboard = $('#testTitle');
+var playerCount = 0;
 
+//===============================================================================
+
+function tooManyPlayers() {
+	alert("Too many players! :P");
+}
+
+function addPlayer(x) {
+	console.log("player " + x + ": " + $('#usernameInput').val().trim());
+	var player = $('#usernameInput').val().trim();
+	if ($("input[type='checkbox']").is(':checked')) {
+		localStorage.setItem('myName', player);
+		sessionStorage.setItem('myName', player);
+	} else {
+		sessionStorage.setItem('myName', player);
+	}
+	if (x === 1) {
+		database.ref().set({
+			player1: player
+		})
+	} else if (x === 2) {
+		database.ref().set({
+			player2: player
+		})
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//================================================================================
+
+// Test counter click
 $('#counterButton').on("click", function() {
+	event.preventDefault();
 	clickCounter++;
 	database.ref().set({
 		clickCount: clickCounter
 	})
 })
+
+$('#playerNameSubmit').on('click', function() {
+	event.preventDefault();
+	if ($('#usernameInput').val().trim() !== "") {
+		if (playerCount === 0) {
+			addPlayer(1);
+			playerCount++;
+		}
+		else if (playerCount === 1) {
+			addPlayer(2);
+			playerCount++; 
+		}
+		else {
+			tooManyPlayers();
+		}
+	}
+	else {
+		alert("null value");
+	}
+})
+
+
+
+
 
 database.ref().on("value", function(snapshot) {
 	console.log(snapshot.val());
