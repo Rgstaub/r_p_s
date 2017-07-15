a1c44d2b81e39ffdbce9584473d9922c008ed8e0
Rgstaub <rgstaub@gmail.com>
2017-07-15 08:29:00 -0400

    ...

--- js/script.js
+++ js/script.js
@@ -25,30 +25,48 @@ var config = {
 firebase.initializeApp(config);
 
 // Set global variables
-var database = firebase.database();
-var playerCount = 0;
-var signedIn = false;
+const database = firebase.database();
+var openSpots;
+const openSpotsRef = database.ref('openSpots');
+const playersRef = database.ref('/players');
+
 var player = {};
 var opponent = {};
 var playerIDs = [];
+var host = false;
 
-var players = database.ref('/connectedPlayers');
-var myPosition = "";
-var playerA_id = "";
-var playerB_id = ""; 
+
+//var myPosition = "";
+// var path = "/players/";
+// var myPath = "";
+// var playerA_id = "";
+// var playerB_id = ""; 
 
 // reset the database
-function initialize() {
-	database.ref().set(null);
-	database.ref('/players').set(playerIDs);
-	reload();
-}
+// function init() {
+// 	database.ref().set(null);
+// 	database.ref('/players').set(playerIDs);
+// 	reload();
+// }
+
+openSpotsRef.on('value', function(snap) {
+	openSpots = snap.val();
+	console.log(openSpots);
+})
+
+database.ref('/players').on('value', function(snap) {
+	var playerArr = Object.keys(snap.val());
+	console.log(playerArr);
+	playerIDs = playerArr;
+})
 
 // Check if the player is signed in
 function signInCheck() {
-	if (sessionStorage.myID) {
-		console.log("Temporarily signed in as ");
-		var signedIn = true;
+	if (sessionStorage.myID || localStorage.myID) {
+		console.log("signed in as ");
+		if (!sessionStorage.myID) {
+			sessionStorage.myID = localStorage.myID;
+		}
 		database.ref("/users/" + sessionStorage.myID).on('value', function(snap) {
 			var name = snap.val().name;
 			console.log(name);
@@ -57,9 +75,9 @@ function signInCheck() {
 			setPlayer();
 		})
      
-	} else if (localStorage.myID) {
-		console.log("Permanently signed in as ");
-		var signedIn = true;
+	// } else if (localStorage.myID) {
+	// 	console.log("Permanently signed in as ");
+	// 	var signedIn = true;
 	} else {
 		$('#nameInput').toggleClass("hidden");
 		$('#signOut').toggleClass("hidden");
@@ -68,27 +86,60 @@ function signInCheck() {
 }
 
 function setPlayer() {
-	if(playerIDs.length === 0) {
-		database.ref('/players/one').set(player.key);
-		myPosition = "one";
-	} else if (playerIDs.length === 1) {
-		database.ref('/players/two').set(player.key);
-		myPosition = "two"
-	} else {
-		alert("All spots are full. Please wait");
+	if (openSpots === 2) {
+		host = true;
+		openSpots--;
+		openSpotsRef.set(openSpots);
+		var tempKey = database.ref('/players').child(player.key).set(player.name);
+		startGame();
+	}
+	else if (openSpots > 0) {
+		openSpots--;
+		openSpotsRef.set(openSpots);
+		var tempKey = database.ref('/players').child(player.key).set(player.name);
+		joinGame();
+	}
+	else {
+		alert("No open Spots");
+		return;
 	}
 }
 
-database.ref('/players').onDisconnect().update({myPosition: null});
+function removePlayer() {
 
-database.ref('/playerA').on('value', function(snap) {
-	playerA_id = snap.val();
 }
-})
 
-database.ref('/playerB').on('value', function(snap) {
-	playerB_id = snap.val();
-})
+function startGame() {
+
+}
+
+function shoot(myThrow, oppThrow) {
+
+}
+// 	if(playerIDs.length === 0) {
+// 		database.ref('/players/one').set(player.key);
+// 		myPosition = "one";
+// 		myPath = "/players/" + myPosition;
+// 	} else if (playerIDs.length === 1) {
+// 		database.ref('/players/two').set(player.key);
+// 		myPosition = "two"
+// 	} else {
+// 		alert("All spots are full. Please wait");
+// 	}
+// 
+
+// console.log(myPath);
+// var thing = database.ref(myPath);
+// thing.onDisconnect().set(null);
+
+// database.ref('/playerA').on('value', function(snap) {
+// 	playerA_id = snap.val();
+// })
+
+
+// database.ref('/playerB').on('value', function(snap) {
+// 	playerB_id = snap.val();
+// })
 
 // If signed in, check if there are open spots in the game
 
@@ -97,7 +148,16 @@ database.ref('/playerB').on('value', function(snap) {
 
 
 // if open spots exist, put player into the game and update number of open spots
+/* 
+	openSpots variable to be increment on each join/disconnect
+	Build an array of the keys of players added to the database
+	check the array for the id that doesn't match your own. Set that id to opponent
+	lookup and display opponent info
+	add the rps interface
 
+
+
+*/
 
 // If no open spots exist, display message and wait for open spot
 
@@ -129,9 +189,6 @@ $('#playerNameSubmit').on('click', function() {
 		paperCount: 0,
 		scissorsCount: 0
 	}
-	console.log(newPlayer.name);
-
-
 
 	// Add the newPlayer to the database and store the key
 	var key = database.ref('/users').push(newPlayer).key;
@@ -147,7 +204,6 @@ $('#playerNameSubmit').on('click', function() {
 	}
 
 	database.ref('/users/' + key).on('value', function(snap) {
-		console.log(snap.val().name);
 		$('#playerNameDisplay').text(snap.val().name);
 		database.ref('/users/' + key + '/key').set(key);
 	})
@@ -160,13 +216,13 @@ $('#playerNameSubmit').on('click', function() {
 
 
 
-database.ref('/players').on("value", function(snap) {
-	if (snap.val().one) {
-		playerIDs[0] = snap.val().one;
-	} if (snap.val().two) {
-		playerIDs[1] = snap.val().two;
-	}
-})
+// database.ref('/players').on("value", function(snap) {
+// 	if (snap.val().one) {
+// 		playerIDs[0] = snap.val().one;
+// 	} if (snap.val().two) {
+// 		playerIDs[1] = snap.val().two;
+// 	}
+// })
 
 // check and log the player in if possible on load
 signInCheck();
@@ -178,8 +234,8 @@ $('#signOut').on('click', function() {
 	sessionStorage.removeItem("myID");
 	// clear out the new username input field
 	$('#usernameInput').val("");
-	var position = playerIDs.indexOf(player.key);
-	console.log(position);
-	playerIDs.splice(position, 1);
+	//var position = playerIDs.indexOf(player.key);
+	//console.log(position);
+	//playerIDs[position] = "";
 	signInCheck();
 })
