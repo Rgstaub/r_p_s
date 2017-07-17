89387b3aad1d9b8da391a7ca6abb760b9fcfe5c4
Rgstaub <rgstaub@gmail.com>
2017-07-17 15:05:56 -0400

    added chat

--- js/script.js
+++ js/script.js
@@ -1,17 +1,5 @@
-/*
 
-Connect the two players to the app
-		Have player enter their name
-		name is saved to local storage
-		db 'logs in' player from name in local storage
-		db checks for opponent and sets that in local storage
-
-Get the player input and hold it until both players have submitted
-
-1
-
-*/
-//=============================================================================
+//========FIREBASE===========================================================
 
 // Initialize Firebase
 var config = {
@@ -24,10 +12,13 @@ var config = {
 };
 firebase.initializeApp(config);
 
+// declare glabal variables and constants
 const dbRef = firebase.database();
 const playerRef = dbRef.ref('players');
-var myPos = ""
+var myPos = "";
+var myName = "";
 
+//========FUNCTIONS=========================================================
 
 // reset the message board and game variables for a new game
 function newGame() {
@@ -41,11 +32,15 @@ function newGame() {
 	}, 5000)
 }
 
+//========EVENT LISTENERS====================================================
+
+// LOGIN AT BEGINNING OF GAME
 // Add a new player when a name is submitted
 $('#playerNameSubmit').on('click', function() {
-	event.preventDefault();  
+	event.preventDefault(); 
+	$('#chat').removeClass('hidden'); 
 	if ($('#usernameInput').val().trim()) {
-		var myName = $('#usernameInput').val().trim();
+		myName = $('#usernameInput').val().trim();
 		var newPlayer = {
 			name: myName,
 			wins: 0,
@@ -73,6 +68,7 @@ $('#playerNameSubmit').on('click', function() {
 	} else {return};
 })
 
+// PICKING R, P, OR S
 //collect the button input from player 1 and update the DB
 $(document).on('click', '.player1', function() {
 	dbRef.ref('players/playerA').update({
@@ -156,9 +152,22 @@ $(document).on('click', '.player2', function() {
 			}
 		}
 	})
-	
+	// Start a new game after p2 makes their selection
 	newGame();
 })
+
+// ADD TO CHAT
+// When chat text is submitted, ad to 'lastMessage' in the database
+$(document).on('click', '#chatSubmit', function() {
+	event.preventDefault();
+	var message = $('#chatText').val().trim();
+	$('#chatText').val(null);
+	if (message) {
+		dbRef.ref('lastMessage').set(myName + ": " + message);
+	}
+})
+
+//===========DATABASE LISTENERS==========================================================
 
 // Detect connected players
 playerRef.on('value', function(snap) {
@@ -174,33 +183,44 @@ dbRef.ref().on('value', function(snap) {
 	var turn = snap.val().turn;
 	var winner = snap.val().winner;
 	var sv = snap.val()
+	// Turn Z is the state when the game is over
 	if (turn === 'z') {
 		$('#winner').text(sv.winner + " Wins!");
 		$('#player1pick').text(sv.players.playerA.name + " picked " + sv.players.playerA.rps);
 		$('#player2pick').text(sv.players.playerB.name + " picked " + sv.players.playerB.rps);
 	}
+	// Turn N is a new game / before the game starts
 	if (turn === 'n') {
 		$('#nameInput').removeClass('hidden');
 		$('#player1').addClass('hidden');
 		$('#player2').addClass('hidden');
+		$('#chat').addClass('hidden');
 		newGame();
 	}
+	// Turn B is the second player's turn.
 	if (turn === 'b') {
 		if (myPos === 'playerB') {
 			$('#p2buttons').removeClass('hidden');
 		}
 	}
+	
+	// Turn A is the first player's turn.
 	if (turn === 'a') {
 		if (myPos === 'playerA') {
 			$('#p1buttons').removeClass('hidden');
 			$('#winner').text("");
 			$('#player1pick').text("");
 			$('#player2pick').text("");
-
 		}
 	}
 })
 
+// DB listener for lastMessage changes to append any new message to the chat log
+dbRef.ref('lastMessage').on('value', function(snap) {
+	var chat = $('<p>');
+	chat.text(snap.val());
+	$('#chatBox').prepend(chat);
+})
 
 
 // reset the game when one player disconnects
@@ -218,289 +238,8 @@ dbRef.ref().onDisconnect().update({
 		}
 	},
 	turn: "n",
-	winner: ""
+	winner: "",
+	lastMessage: ""
 });
 
 
-
-// function init() {
-// 	playerRef.set({
-// 		playerA: {
-// 			name: "",
-// 			wins: "0",
-// 			rps: "dynamite"
-// 		},
-// 		playerB: {
-// 			name: "",
-// 			wins: "0",
-// 			rps: "dynamite"
-// 		}
-// 	})
-// 	refresh();
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-// Set global variables
-const database = firebase.database();
-var openSpots;
-const openSpotsRef = database.ref('openSpots');
-const playersRef = database.ref('/players');
-
-var player = {};
-var opponent = {};
-var playerIDs = [];
-var host = false;
-
-
-//var myPosition = "";
-// var path = "/players/";
-// var myPath = "";
-// var playerA_id = "";
-// var playerB_id = ""; 
-
-// reset the database
-// function init() {
-// 	database.ref().set(null);
-// 	database.ref('/players').set(playerIDs);
-// 	reload();
-// }
-
-openSpotsRef.on('value', function(snap) {
-	openSpots = snap.val();
-	console.log(openSpots);
-})
-
-database.ref('/players').on('value', function(snap) {
-	var playerArr = Object.keys(snap.val());
-	console.log(playerArr);
-	playerIDs = playerArr;
-})
-
-// Check if the player is signed in
-function signInCheck() {
-	if (sessionStorage.myID || localStorage.myID) {
-		console.log("signed in as ");
-		if (!sessionStorage.myID) {
-			sessionStorage.myID = localStorage.myID;
-		}
-		database.ref("/users/" + sessionStorage.myID).on('value', function(snap) {
-			var name = snap.val().name;
-			console.log(name);
-			$('#playerNameDisplay').text(name);
-			player = snap.val();
-			setPlayer();
-		})
-     
-	// } else if (localStorage.myID) {
-	// 	console.log("Permanently signed in as ");
-	// 	var signedIn = true;
-	} else {
-		$('#nameInput').toggleClass("hidden");
-		$('#signOut').toggleClass("hidden");
-		$('#playerNameDisplay').text("Sign in with a Player Name");
-	}
-}
-
-function setPlayer() {
-	if (openSpots === 2) {
-		host = true;
-		openSpots--;
-		openSpotsRef.set(openSpots);
-		var tempKey = database.ref('/players').child(player.key).set(player.name);
-		startGame();
-	}
-	else if (openSpots > 0) {
-		openSpots--;
-		openSpotsRef.set(openSpots);
-		var tempKey = database.ref('/players').child(player.key).set(player.name);
-		joinGame();
-	}
-	else {
-		alert("No open Spots");
-		return;
-	}
-}
-
-function removePlayer() {
-
-}
-
-function startGame() {
-
-}
-
-function shoot(myThrow, oppThrow) {
-
-}
-// 	if(playerIDs.length === 0) {
-// 		database.ref('/players/one').set(player.key);
-// 		myPosition = "one";
-// 		myPath = "/players/" + myPosition;
-// 	} else if (playerIDs.length === 1) {
-// 		database.ref('/players/two').set(player.key);
-// 		myPosition = "two"
-// 	} else {
-// 		alert("All spots are full. Please wait");
-// 	}
-// 
-
-// console.log(myPath);
-// var thing = database.ref(myPath);
-// thing.onDisconnect().set(null);
-
-// database.ref('/playerA').on('value', function(snap) {
-// 	playerA_id = snap.val();
-// })
-
-
-// database.ref('/playerB').on('value', function(snap) {
-// 	playerB_id = snap.val();
-// })
-
-// If signed in, check if there are open spots in the game
-
-
-// If not signed in, display the signin form
-
-
-// if open spots exist, put player into the game and update number of open spots
-/* 
-	openSpots variable to be increment on each join/disconnect
-	Build an array of the keys of players added to the database
-	check the array for the id that doesn't match your own. Set that id to opponent
-	lookup and display opponent info
-	add the rps interface
-
-
-
-*/
-
-// If no open spots exist, display message and wait for open spot
-
-
-// Once in the game, check if there is an opponent. If opponent exists, begin the game
-
-
-// Get choice from each player, determine outcome, append results to page
-
-
-//===============================================================================
-
-
-
-//	ADD PLAYER MODULE
-//================================================================================
-
-// Handle a new player submission
-/*
-$('#playerNameSubmit').on('click', function() {
-	event.preventDefault();
-
-	//create a new player object
-	var newPlayer = {
-		name: $('#usernameInput').val().trim(),
-		key: "placeholder",
-		wins: 0,
-		losses: 0,
-		rockCount: 0,
-		paperCount: 0,
-		scissorsCount: 0
-	}
-
-	// Add the newPlayer to the database and store the key
-	var key = database.ref('/users').push(newPlayer).key;
-	newPlayer.key = key;
-	
-	// add the player's id (key) to session storage or local storage if "remember me" is checked
-	if ($("input[type='checkbox']").is(':checked')) {
-		localStorage.setItem('myID', key);
-		sessionStorage.setItem('myID', key);
-	} else {
-		sessionStorage.setItem('myID', key);
-		localStorage.removeItem('myID');
-	}
-
-	database.ref('/users/' + key).on('value', function(snap) {
-		$('#playerNameDisplay').text(snap.val().name);
-		database.ref('/users/' + key + '/key').set(key);
-	})
-
-	$('#nameInput').toggleClass("hidden");
-	$('#signOut').toggleClass("hidden");
-	signInCheck();
-})
-
-
-
-
-// database.ref('/players').on("value", function(snap) {
-// 	if (snap.val().one) {
-// 		playerIDs[0] = snap.val().one;
-// 	} if (snap.val().two) {
-// 		playerIDs[1] = snap.val().two;
-// 	}
-// })
-
-// check and log the player in if possible on load
-signInCheck();
-
-
-
-$('#signOut').on('click', function() {
-	localStorage.removeItem("myID");
-	sessionStorage.removeItem("myID");
-	// clear out the new username input field
-	$('#usernameInput').val("");
-	//var position = playerIDs.indexOf(player.key);
-	//console.log(position);
-	//playerIDs[position] = "";
-	signInCheck();
-})
-*/
